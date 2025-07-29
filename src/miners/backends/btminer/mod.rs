c66200fe4433120f2faa6bbe116808897dbfc4d1
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2025-07-29 16:06:20 -0600

    refactor: make ESPMiner creation more consistent with whatsminer design

---
+++ src/miners/backends/btminer/mod.rs
@@ -0,0 +1,29 @@
+use std::net::IpAddr;
+
+pub use btminer_3::BTMiner3;
+use semver;
+
+use crate::data::device::{MinerFirmware, MinerModel};
+use crate::miners::backends::traits::GetMinerData;
+
+pub mod btminer_3;
+
+pub struct BTMiner {}
+
+impl BTMiner {
+    pub fn new(
+        ip: IpAddr,
+        model: MinerModel,
+        firmware: MinerFirmware,
+        version: semver::Version,
+    ) -> Box<dyn GetMinerData> {
+        if semver::VersionReq::parse(">=2024.11.0")
+            .unwrap()
+            .matches(&version)
+        {
+            Box::new(BTMiner3::new(ip, model, firmware))
+        } else {
+            panic!("Unsupported BTMiner version")
+        }
+    }
+}
