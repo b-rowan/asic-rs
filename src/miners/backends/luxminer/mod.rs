02c06c8f4586fbbf3c5522da2a16e1b62cb5a430
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2025-09-22 08:18:45 -0600

    bug: add luxminer control so tests dont fail

--- src/miners/backends/luxminer/mod.rs
+++ src/miners/backends/luxminer/mod.rs
@@ -1,5 +1,5 @@
 use crate::data::device::MinerModel;
-use crate::miners::backends::traits::GetMinerData;
+use crate::miners::backends::traits::Miner;
 use std::net::IpAddr;
 use v1::LuxMinerV1;
 
@@ -9,7 +9,7 @@ pub struct LuxMiner;
 
 impl LuxMiner {
     #[allow(clippy::new_ret_no_self)]
-    pub fn new(ip: IpAddr, model: MinerModel, _: Option<semver::Version>) -> Box<dyn GetMinerData> {
+    pub fn new(ip: IpAddr, model: MinerModel, _: Option<semver::Version>) -> Box<dyn Miner> {
         Box::new(LuxMinerV1::new(ip, model))
     }
 }
--- src/miners/backends/luxminer/v1/mod.rs
+++ src/miners/backends/luxminer/v1/mod.rs
@@ -9,7 +9,7 @@ use crate::miners::commands::MinerCommand;
 use crate::miners::data::{
     DataCollector, DataExtensions, DataExtractor, DataField, DataLocation, get_by_pointer,
 };
-use anyhow::{Result, anyhow};
+use anyhow::{Result, anyhow, bail};
 use async_trait::async_trait;
 use macaddr::MacAddr;
 use measurements::{AngularVelocity, Frequency, Power, Temperature, Voltage};
@@ -876,5 +876,44 @@ impl GetMessages for LuxMinerV1 {
                 })
             })
             .collect()
+    }
+}
+
+#[async_trait]
+impl SetFaultLight for LuxMinerV1 {
+    #[allow(unused_variables)]
+    async fn set_fault_light(&self, fault: bool) -> Result<bool> {
+        bail!("Unsupported command");
+    }
+}
+
+#[async_trait]
+impl SetPowerLimit for LuxMinerV1 {
+    #[allow(unused_variables)]
+    async fn set_power_limit(&self, limit: Power) -> Result<bool> {
+        bail!("Unsupported command");
+    }
+}
+
+#[async_trait]
+impl Restart for LuxMinerV1 {
+    async fn restart(&self) -> Result<bool> {
+        bail!("Unsupported command");
+    }
+}
+
+#[async_trait]
+impl Pause for LuxMinerV1 {
+    #[allow(unused_variables)]
+    async fn pause(&self, at_time: Option<Duration>) -> Result<bool> {
+        bail!("Unsupported command");
+    }
+}
+
+#[async_trait]
+impl Resume for LuxMinerV1 {
+    #[allow(unused_variables)]
+    async fn resume(&self, at_time: Option<Duration>) -> Result<bool> {
+        bail!("Unsupported command");
     }
 }
